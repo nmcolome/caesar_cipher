29b0b8c43f9fdf02511857bcb9a92441baa5c273
NataliaColome <nmcolome@gmail.com>
2018-07-16 19:43:40 -0600

    Add test for alphabet and .encode

--- test/caesar_cipher_test.rb
+++ test/caesar_cipher_test.rb
@@ -1,5 +1,20 @@
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
-require '../lib/ceasar_cipher.rb'
+require '../lib/caesar_cipher.rb'
 
+class CaesarCipherTest < Minitest::Test
+  def setup
+    @cipher = CaesarCipher.new
+  end
+
+  def test_it_generates_alphabet
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    assert_equal alphabet, @cipher.abc
+  end
+
+  def test_it_encodes_message
+    assert_equal "CZGGJ RJMGY", @cipher.encode("Hello World", 5)
+  end
+end
